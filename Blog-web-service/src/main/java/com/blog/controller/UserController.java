bdd58d503ce127894f29fd7c95a4603988046091
音老怪 <m13855029721@163.com>
2019-11-01 10:13:36 +0800

    feat(User): 新增邮箱激活功能

--- Blog-web-service/src/main/java/com/blog/controller/UserController.java
+++ Blog-web-service/src/main/java/com/blog/controller/UserController.java
@@ -1,22 +1,29 @@
 package com.blog.controller;
 
+import com.alibaba.druid.Constants;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.blog.pojo.User;
 import com.blog.service.UserService;
+import com.blog.utils.MailUtils;
 import com.blog.utils.SmsUtil;
 import com.blog.vo.BaseResult;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author JeungNyeongJae
+ * @author Yinchao
  */
 
 @RestController
@@ -29,6 +36,14 @@ public class UserController {
     @Resource
     private StringRedisTemplate redisTemplate;
 
+    @Resource
+    private HttpSession httpSession;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+
+
     /**
      * 注册保存用户信息
      * @param user // 用户
@@ -37,11 +52,11 @@ public class UserController {
     @PostMapping("/register")
     private ResponseEntity<Object> rigister(@RequestBody User user){
 
+
         this.userService.saveUser(user);
 
         return ResponseEntity.ok( new BaseResult(0 , "注册成功"));
     }
-
     /**
      * 注册时发送验证短信
      * @param user // 用户
@@ -71,6 +86,54 @@ public class UserController {
             return ResponseEntity.ok(new BaseResult( 1 , "发送失败"));
         }
     }
+    /**
+     * 注册时发送邮箱激活账号
+     * @param user // 用户
+     * @return
+     */
+    public ResponseEntity<Void> regist(String checkcode, User user){
+        //校验验证码
+        String code = httpSession.getAttribute(user.getUserMobile()).toString();
+        if(code!=null&&!"".equals(code.trim())){
+            if(code.equals(checkcode)){
+                //移除激活码
+                httpSession.removeAttribute(user.getUserMobile());
+                //调用服务
+                String url = "http://localhost:8088";   //假的链接
+                // 第一个参数：url
+                // 第二个参数：数据
+                // 第三个参数：返回值类型
+                ResponseEntity<String>entity =restTemplate.postForEntity(url,user,String.class);
+                /******************发送邮件******************/
+                /**
+                 * 1 产生activeCode
+                 * 2 将activeCode保存到redis中
+                 *  注意“地址栏的参数中不能带-
+                 * 3 拼接url <a href="localhost:8092/regist/activeMail?telephone=xxx&activeCode=xxxx-sss-aaa">速运快递账号激活</a>
+                 */
+                String activeCode = UUID.randomUUID().toString().replace("-","");
+                // 将code 放到redis中
+                // 第三个参数：时间
+                // 第四个参数：单位
+                redisTemplate.opsForValue().set(user.getUserMobile(), activeCode,1,TimeUnit.DAYS);
+                //发送邮件
+                String activeUrl = "http://localhost:8088" +  //链接假的
+                        "/regist/activeMail?telephone="+user.getUserMobile()+"&activeCode="+activeCode;
+                String content = "<a href='"+activeUrl+"'>百合网账号激活链接</a>";
+                try {
+                    MailUtils.sendMail(user.getUserEmail(),"博客网账号激活",content);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                HttpStatus statusCode = entity.getStatusCode();
+                return new ResponseEntity<>(statusCode);
+
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
     /**
      * 通过手机号和密码进行查询
@@ -92,4 +155,7 @@ public class UserController {
 
         return ResponseEntity.ok( user );
     }
+
+
+
 }
